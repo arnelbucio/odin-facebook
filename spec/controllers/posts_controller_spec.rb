1546ff0cf3b0f53dd9b8ba1ecf932b69c14e1c35
Arnel Bucio <rnlbucio@gmail.com>
2014-08-28 21:56:44 +0800

    Add post model tests

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,12 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe PostsController, :type => :controller do
-
-  describe "GET index" do
-    it "returns http success" do
-      get :index
-      expect(response).to be_success
-    end
-  end
-
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe Post, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Post, type: :model do
+  describe 'associations' do
+    it { should belong_to(:user) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:content) }
+  end
 end
