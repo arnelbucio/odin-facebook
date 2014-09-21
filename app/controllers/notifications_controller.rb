36cd8ad2f0a1f739bf42e41d9fd1460d237da080
Arnel Bucio <rnlbucio@gmail.com>
2014-09-21 10:09:53 +0800

    Paginate notifications

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,6 +1,6 @@
 class NotificationsController < ApplicationController
   def index
-    @notifications = current_user.notifications
+    @notifications = current_user.notifications.page(params[:page]).per(30)
     @notifications.pending.update_all(seen: true)
   end
 end
