5d230e101f180d3473cfc4ba20b2e918a06b10de
Arnel Bucio <rnlbucio@gmail.com>
2014-09-27 17:49:58 +0800

    Add bootstrap-collapse.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery_ujs
 //= require bootstrap/alert
 //= require bootstrap/tab
+//= require bootstrap/collapse
 //= require jquery.remotipart
 //= require rails-timeago
 //= require turbolinks
